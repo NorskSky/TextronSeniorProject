ffcb9c71f23999595f12d7f3643462dc0b708d82
NorskSky <151588842+NorskSky@users.noreply.github.com>
2024-04-16 14:15:31 -0500

    added Builder to load kv file

--- GUI/main.py
+++ GUI/main.py
@@ -2,8 +2,12 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.land.builder import Builder
 class MAINWINDOW(Widget):
     pass
+
+Builder.load_file("tester.kv")
+
 class TESTERGUI(App):
     def build(self):
         return MAINWINDOW()
