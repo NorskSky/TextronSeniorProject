d7a7eeb30b406cba8b825a3d97757f949a8cb62d
NorskSky <151588842+NorskSky@users.noreply.github.com>
2024-04-27 22:20:56 -0500

    Moved .kv files into a subfolder, started wokring on the results screen

--- GUI/main.py
+++ GUI/main.py
@@ -1,7 +1,6 @@
-from kivy.uix.accordion import Widget
-from kivy.uix.accordion import Animation
 import kivy
-import queue, threading, random,time,os
+import threading, time
+#from pathlib import Path, PurePath
 
 #Loads the config file
 from kivy.config import Config
@@ -13,30 +12,60 @@ from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.animation import Animation
 from kivy.properties import NumericProperty
-from kivy.properties import ObjectProperty
+from kivy.uix.accordion import Widget
+from kivy.uix.accordion import Animation
 from kivy.uix.popup import Popup
 from kivy.factory import Factory
+from kivy.properties import ObjectProperty
 
 #Builder.load_file("tester.kv")
 
 #Loads the kv files, that dictate look, feel, and behavior of the GUI 
-Builder.load_file("menu.kv")
-Builder.load_file("settings.kv")
-Builder.load_file("loading.kv")
+Builder.load_file("kv_files/menu.kv")
+Builder.load_file("kv_files/settings.kv")
+Builder.load_file("kv_files/loading.kv")
+Builder.load_file("kv_files/results.kv")
 
 class MENUSCREEN(Screen):
     pass
 
 
-class SETTINGSSCREEN(Screen):      
-    pass
+class SETTINGSSCREEN(Screen):     
+   pass
+    
 
 class RESULTSSCREEN(Screen):
-    pass
+    Outputs = [ObjectProperty]*8 
+    Results = [ObjectProperty]*8     
+        
+    
+    def on_pre_enter(self):
+        self.Outputs[0] = self.ids.Output1
+        self.Outputs[1] = self.ids.Output2
+        self.Outputs[2] = self.ids.Output3
+        self.Outputs[3] = self.ids.Output4
+        self.Outputs[4] = self.ids.Output5
+        self.Outputs[5] = self.ids.Output6
+        self.Outputs[6] = self.ids.Output7
+        self.Outputs[7] = self.ids.Output8
+        
+        for i in range(0,8):            
+            if self.Outputs[i].text != "Not Selected |":
+                self.Outputs[i].color = (0,0,0,1)
+            
+        print("on pre enter for results has been entered")
+    
+    def updateOutputs(self, results):
+        for i in range(0, 8):                 
+            if results[i] != "Not Selected":                
+                self.Outputs[i].text = results[i] + ' @ FTM Input ' + str(i+1) + ' |' 
+            
+    
 
 class LOADING(Popup):    
     pass
 
+#Both the SPINRECTANGLE classes are use to create the loading screen animation
 class SPINRECTANGLE(Widget):
     angle = NumericProperty(0)
     def __init__(self, **kwargs):
@@ -61,34 +90,59 @@ class SPINRECTANGLE2(Widget):
         if angle == -360:
             item.angle = 0
 
-  
-    
+class RESULTSSTORAGE():    
+    data1 = str()
+    data2 = str()
+    data3 = str()
+    data4 = str()
+    data5 = str()
+    data6 = str()
+    data7 = str()
+    data8 = str()
+    def __init__(self):
+        self.data1 = "Default Data"
+        self.data2 = "Default Data"
+        self.data3 = "Default Data"
+        self.data4 = "Default Data"
+        self.data5 = "Default Data"
+        self.data6 = "Default Data"
+        self.data7 = "Default Data"
+        self.data8 = "Default Data"
+        
+            
 
-class TESTERGUI(App):    
-    sm = ScreenManager()
-    sineWavePt = list()
-    isDone = -1
-    
-    #Data used in the .kv files for color and other uses
-    INPUTS = ('GMA Output 1', 'GMA Output 2', 'GMA Output 3', 'GMA Output 4', 'GMA Output 5', 'GMA Output 6', 'GMA Output 7', 'GMA Output 8')
+class COLORS():
     STEELBLUE = (0.2471, 0.5333, 0.7726, 1)
     PINEGREEN = (0.0745, 0.4353, 0.3882, 1)
     ENGORANGE = (0.8157, 0.0000, 0.0000, 1)
     SELYELLOW = (1.0000, 0.7294, 0.0314, 1)
     PRUSSBLUE = (0.0118, 0.1686, 0.2627, 1)
-    Thresholds = list()
+    WHITE = (1.0000, 1.0000, 1.000, 1)
+    BLACK = (0.0000, 0.0000, 0.000, 1)
+    
+
+class TESTERGUI(App):    
+    sm = ScreenManager()
+    #Data used in the .kv files for color and other uses
+    colors = COLORS()
+    results = RESULTSSTORAGE()  
+      
+    INPUTS = ('GMA Output 1', 'GMA Output 2', 'GMA Output 3', 'GMA Output 4', 'GMA Output 5', 'GMA Output 6', 'GMA Output 7', 'GMA Output 8')
+    Thresholds = [""] * 8
+    OutputsSelected = ["Not Selected"] * 8
    
    
     def wasteTime(self):
-        thistime = time.time()         
-        while thistime + 5 > time.time(): # 5 seconds
-            time.sleep(1)
-        self.isDone = 1
-        print("time wasting done")
-        self.pop_up.dismiss()
         
+        thistime = time.time()         
+        '''while thistime + 5 > time.time(): # 5 seconds
+            time.sleep(1)'''
+       
+        print("time wasting done")              
+        self.pop_up.dismiss()           
     
-    
+ 
+    #Pops up the loading screen while the analysis happens
     def showLoading(self):       
         self.pop_up = Factory.LOADING()        
         self.pop_up.open()
@@ -96,40 +150,54 @@ class TESTERGUI(App):
     #Defines the Behavior when the Start Test button is pressed
     #This button is located with in the 'menu' screen
     def startTest(self, text):
-        print(text)
-        self.showLoading()
-        mythread = threading.Thread(target=self.wasteTime)
-        mythread.start()
+        print(text)        
+        self.showLoading()       
         
-    
-    def switchResults(self,otherThread):
-        otherThread.join()
-        print("Back to menu")
-        self.sm.current = 'menu'
-           
+        mythread = threading.Thread(target=self.wasteTime)
+        mythread.start()  
+        self.sm.current = 'results' 
+        
+        
     
     #Defines the Behavior when the Export button is pressed
     #This button is located with in the 'menu' screen
     def export(self, text):
         print(text)
     
-    def save(self):           
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_1.text)    
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_2.text)
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_3.text)
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_4.text)
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_5.text)
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_6.text)
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_7.text)
-        self.Thresholds.append(self.sm.current_screen.ids.thresholdInput_8.text)      
-        for i in self.Thresholds:
-            print(i)       
+    #Defined the Behavior of the Save button located in the Settings Screen
+    #Save test parameters to a set of data structures
+    def save(self):
+                   
+        #saving Threshold Values        
+        self.Thresholds[0] = (self.sm.current_screen.ids.thresholdInput_1.text)    
+        self.Thresholds[1] = (self.sm.current_screen.ids.thresholdInput_2.text)
+        self.Thresholds[2] = (self.sm.current_screen.ids.thresholdInput_3.text)
+        self.Thresholds[3] = (self.sm.current_screen.ids.thresholdInput_4.text)
+        self.Thresholds[4] = (self.sm.current_screen.ids.thresholdInput_5.text)
+        self.Thresholds[5] = (self.sm.current_screen.ids.thresholdInput_6.text)
+        self.Thresholds[6] = (self.sm.current_screen.ids.thresholdInput_7.text)
+        self.Thresholds[7] = (self.sm.current_screen.ids.thresholdInput_8.text)
+       
+        #Saving GMA Selected Outputs        
+        self.OutputsSelected[0] = (self.sm.current_screen.ids.dropdown_1.text)    
+        self.OutputsSelected[1] = (self.sm.current_screen.ids.dropdown_2.text)
+        self.OutputsSelected[2] = (self.sm.current_screen.ids.dropdown_3.text)
+        self.OutputsSelected[3] = (self.sm.current_screen.ids.dropdown_4.text)
+        self.OutputsSelected[4] = (self.sm.current_screen.ids.dropdown_5.text)
+        self.OutputsSelected[5] = (self.sm.current_screen.ids.dropdown_6.text)
+        self.OutputsSelected[6] = (self.sm.current_screen.ids.dropdown_7.text)
+        self.OutputsSelected[7] = (self.sm.current_screen.ids.dropdown_8.text)         
+        
+        #Updates the result screen with the correct data 
+        self.sm.current = 'results'
+            
+        self.sm.get_screen('results').updateOutputs(self.OutputsSelected)
         self.sm.current = 'menu'
         
     def build(self):          
         self.sm.add_widget(MENUSCREEN(name = 'menu'))   
         self.sm.add_widget(SETTINGSSCREEN(name = 'settings'))     
-        #self.sm.add_widget(LOADINGSCREEN(name = 'loading'))            
+        self.sm.add_widget(RESULTSSCREEN(name = 'results'))    
         return self.sm
     
 if __name__ == '__main__':
